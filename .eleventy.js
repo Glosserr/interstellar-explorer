b3280533c00e7767bd9ae8173435f97ea1ffe68d
Richard Glosser <146496112+Glosserr@users.noreply.github.com>
2025-02-03 02:42:33 +0000

    Initial Changes!!!

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,22 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
+  // Existing config
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  // Add blog post collection
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("posts/*.md");
+  });
+
+  // Add log preview shortcode
+  eleventyConfig.addShortcode("logPreview", function(post) {
+    return `
+      <article class="log-preview">
+        <h2>${post.data.title}</h2>
+        <div class="log-date">Stellar Date: ${post.data.date}</div>
+        <p>${post.data.excerpt}</p>
+        <a href="${post.url}">Read Full Log →</a>
+      </article>
+    `;
+  });
 };
